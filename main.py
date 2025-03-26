359369c64445683281c69597272afe5cfc7830ee
DARSHAN JETHVA <88070531+DarshanJ2004@users.noreply.github.com>
2025-03-26 22:00:20 +0530

    camera feature added

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("hello world") #comment added
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open camera.")
+    exit()
+
+
+while True:
+    ret, frame = cap.read()
+    
+
+    if not ret:
+        print("Error: Failed to capture frame.")
+        break
+    
+
+    cv2.imshow("Camera Feed", frame)
+    
+    # Press 'q' to close the camera window
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
